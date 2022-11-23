8bdc651832610858694edc172df58b5fdb037216
nespralnic <niconespral@gmail.com>
2022-11-23 13:01:08 -0300

    jirafa, parallax, enhancement del left menu

--- js/leftMenu.js
+++ js/leftMenu.js
@@ -26,7 +26,7 @@ let leftMenutext = document.querySelector(".left-menu-text")
 // secciones participantes
 // ("html tag","message","top start","bottom end")
 let sections = [
-    new Section("destacados","MÁS TRABAJOS!",0,400),
+    new Section("destacados","MÁS TRABAJOS!",0,200),
     //new Section("slogan","hola",-100,0),
     new Section("equipo","Click para BIOs",-100,200)
 ]
@@ -41,24 +41,16 @@ const toggleActive = (obj) =>{
     obj.active = condition ? true : false
 
 }
-/*
 
-window.addEventListener("scroll", ()=>{
-    console.log(window.scrollY + "window position");
-    let position = destacados.getBoundingClientRect().y
-    if (true){
-        
-        console.log(destacados.getBoundingClientRect().y + "inside");
-        console.log(destacados.getBoundingClientRect().height + "altura"); 
-    }
-})
-*/
+justOnceArray = [] //alerts pops up just once
+//initialize div outside the box
+leftMenu.classList.add("left-menu-display")
 
-justOnceArray = [] //alerts pop up just once
 
 window.addEventListener("scroll",()=>{
     let option = sections.find(e => e.active == true)
     Section.active = option ? true : false
+
 
     sections.forEach(e => {
         
@@ -66,9 +58,10 @@ window.addEventListener("scroll",()=>{
         if (e.active && !justOnceArray.includes(e.name)){
             e.once = true
             leftMenutext.innerHTML = e.msg 
-            leftMenu.classList.add("left-menu-display")
+            
+            leftMenu.classList.add("left-menu-appear")
         } else if (!Section.active) {
-            leftMenu.classList.remove("left-menu-display")
+            leftMenu.classList.remove("left-menu-appear")
             if (e.once && !justOnceArray.includes(e.name)){
                 justOnceArray.push(e.name)
             }
--- js/parallax.js
+++ js/parallax.js
@@ -1,8 +1,16 @@
 let parallaxBg = document.getElementById('parallax-bg')
-console.log(parallaxBg);
+let giraffe = document.querySelector('.slogan-img')
+
+//get initial giraffe y
+let giraffeHeight = giraffe.getBoundingClientRect().height
 
 window.addEventListener('scroll', ()=>{
     let value = window.scrollY
-    console.log(parallaxBg.style.top);
     parallaxBg.style.top = value * -0.3 + 'px';
+    let activeGiraffe = (giraffe.getBoundingClientRect().top - giraffe.getBoundingClientRect().height  )*-1
+    
+    if ( activeGiraffe > 0 && activeGiraffe < giraffeHeight ){
+        giraffe.style.left = 55 + activeGiraffe * -0.005 + '%';
+    }
+    
 })
