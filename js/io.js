26a0400c06f71ec09c3595f13a0e344a37102ec9
nespralnic <niconespral@gmail.com>
2023-03-27 23:10:43 -0300

    better titles and footer

--- js/io.js
+++ js/io.js
@@ -1,6 +1,7 @@
 let observer = new IntersectionObserver(handleIntersect,{});
 
 let target = document.querySelectorAll(".section-title-underline");
+
 target.forEach((tgt)=> {
     observer.observe(tgt);
 })
@@ -9,8 +10,8 @@ function handleIntersect(entries){
     entries.forEach((entry) => {
         if (entry.isIntersecting){
             entry.target.classList.add("active");
+            let parent = entry.target.parentElement
+            parent.classList.add('active')
         }})
 }
-
-
 
